3dedf96715c212dc4f899a6b422cc252991248b3
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2025-01-20 23:40:07 -0400

    form setup

--- src/App.js
+++ src/App.js
@@ -15,6 +15,17 @@ class App extends React.Component {
     this.setState({ manager, players, balance });
   }
 
+  onSubmit = async (event) => {
+    event.preventDefault();
+
+    const accounts = await web3.eth.getAccounts();
+
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(this.state.value, "ether"),
+    });
+  };
+
   render() {
     return (
       <div>
@@ -28,7 +39,7 @@ class App extends React.Component {
 
         <hr />
 
-        <form>
+        <form onSubmit={this.onSubmit}>
           <h4>Want to try your luck?</h4>
           <div>
             <label for="">Amount of ether to enter</label>
